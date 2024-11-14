e111460a2a8dae3e46d638f12f859ca5b8c56c6b
226129 <al226129@alumnos.uacj.mx>
2024-11-14 11:13:51 -0700

    Cambios en los modelos, vistas y urls de catalogo

--- backend/apps/catalogo/urls.py
+++ backend/apps/catalogo/urls.py
@@ -1,1 +1,5 @@
-# url a catalogo
+"""
+    url('catalogo/<id_producto>', buscar, name='buscar'),
+    url('catalogo/filtro/', filtrar, name='filtrar'),
+    url('catalogo/', catalogo, name='catalogo'),
+"""
---
+++ backend/ferreteria/settings.py
@@ -0,0 +1,14 @@
+"""
+El archivo `settings.py` en un proyecto de Django es un módulo de configuración central que define las opciones de configuración globales para la aplicación Django. 
+Este archivo contiene una variedad de configuraciones que controlan el comportamiento del proyecto, tales como:
+
+- `DEBUG`: Indica si el modo de depuración está activado.
+- `DATABASES`: Configura las bases de datos utilizadas por el proyecto.
+- `INSTALLED_APPS`: Lista de aplicaciones habilitadas en el proyecto.
+- `MIDDLEWARE`: Lista de middleware que se ejecutan en cada solicitud.
+- `TEMPLATES`: Configuración de los motores de plantillas.
+- `STATIC_URL` y `STATICFILES_DIRS`: Configuración para archivos estáticos.
+- `LANGUAGE_CODE` y `TIME_ZONE`: Configuración de idioma y zona horaria.
+
+El archivo `settings.py` es esencial para personalizar y adaptar el comportamiento del proyecto Django a las necesidades específicas del desarrollo y despliegue.
+"""
