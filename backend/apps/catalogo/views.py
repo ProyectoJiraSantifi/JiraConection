1bb2857f1acc74b6ccaf4e0909c26bed4047bee0
226129 <al226129@alumnos.uacj.mx>
2024-11-14 10:38:46 -0700

    Movi archivos de catalogo a su seccion del proyecto

---
+++ backend/apps/catalogo/views.py
@@ -0,0 +1,16 @@
+# Componentes incluidos:
+# 1. Barra de búsqueda: El usuario podrá ingresar un término de búsqueda, y la plataforma
+#    mostrará productos que coincidan en los campos relevantes, como nombre o descripción.
+#    - Al ingresar una palabra clave, se realizará una consulta a la base de datos, y los
+#      resultados se mostrarán inmediatamente en la página.
+
+# 2. Filtros: Los usuarios podrán aplicar varios filtros simultáneamente para limitar
+#    los resultados según diferentes criterios. Los filtros serán:
+#    - Categoría: Permite filtrar productos por categorías específicas.
+#    - Precio: Los usuarios podrán seleccionar un rango de precios.
+#    - Disponibilidad: Filtra los productos por estado de inventario, como "En stock" o
+#      "Agotado".
+
+# 3. Resultados: Los productos se actualizarán dinámicamente al aplicar filtros o realizar
+#    una búsqueda, sin necesidad de recargar la página completa, optimizando la experiencia
+#    de usuario.
