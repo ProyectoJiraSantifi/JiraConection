567aef55a793fbc3a720c98b222b02bf75359e45
226129 <al226129@alumnos.uacj.mx>
2024-11-13 22:00:01 -0700

    Estructura de proyecto final. Los archivos solo contendran documentacion sobre que se hara en cada seccion

---
+++ backend/ferreteria/urls.py
@@ -0,0 +1,9 @@
+"""
+El archivo `urls.py` en un proyecto de Django recién creado se utiliza para definir las rutas URL que serán manejadas por la aplicación. Este archivo contiene un conjunto de patrones de URL que mapean las solicitudes entrantes a las vistas correspondientes. En un proyecto de Django, el archivo `urls.py` es esencial para la configuración de la navegación y la estructura de la aplicación web.
+
+Funciones principales del archivo `urls.py`:
+- Definir patrones de URL utilizando expresiones regulares o rutas simples.
+- Asociar cada patrón de URL con una vista específica que manejará la solicitud.
+- Incluir otros archivos `urls.py` de aplicaciones individuales para modularizar la configuración de URL.
+
+"""
---
+++ backend/manage.py
@@ -0,0 +1,16 @@
+"""
+Este archivo es el punto de entrada principal para la administración del proyecto Django.
+Proporciona las siguientes funcionalidades:
+
+- Ejecutar el servidor de desarrollo: Permite iniciar el servidor de desarrollo de Django.
+- Ejecutar comandos de administración: Permite ejecutar comandos de administración de Django, como migraciones, creación de superusuarios, etc.
+- Configuración del entorno: Configura el entorno necesario para ejecutar el proyecto Django.
+
+El archivo está estructurado de la siguiente manera:
+1. Importaciones: Importa los módulos necesarios para la ejecución del archivo.
+2. Configuración del entorno: Configura las variables de entorno necesarias para el proyecto Django.
+3. Ejecución del comando: Ejecuta el comando de administración proporcionado por el usuario.
+
+Ejemplo de uso:
+python manage.py runserver
+"""
