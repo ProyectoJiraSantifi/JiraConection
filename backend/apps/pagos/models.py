ea2ef251a89eda830c47e71e6742afc89b622a75
aguilar <al217149@alumnos.uacj.mx>
2024-11-14 22:18:59 -0700

    Se ha agregador la integracion con proveedores de pagos

--- backend/apps/pagos/models.py
+++ backend/apps/pagos/models.py
@@ -18,3 +18,36 @@ Pedido:
 
 
 """
+
+"""
+Modelo de Pago
+ 
+Atributos
+	-idPago: entero
+	-metododePago: string 
+	-monto: float
+	-estado: string
+	-IDUSUARIO: usuario
+ 
+Metodos
+	procesarPago():
+	Realiza el procesamiento del pago mediante la plataforma seleccionada (PayPal o Mercado Libre).	
+	En el caso de PayPal, incluye la autenticación y redirección a la plataforma de PayPal.
+	En el caso de Mercado Libre, se conecta al API correspondiente para procesar el pago.
+	verificarPago(): Verifica si el pago fue exitoso consultando la respuesta de la plataforma (PayPal o Mercado Libre) y actualiza el estado en consecuencia.
+ 
+Conexiones:
+Pedido:
+	Relación uno a uno entre Pedido y Pago: Un pedido puede tener solo un pago asociado, y cada pago corresponde a un único pedido específico.
+ 
+ 
+Implementación específica para PayPal y Mercado Libre:
+procesarPagoConPayPal(): Realiza el procesamiento de pagos a través de PayPal, redirigiendo al usuario y verificando la respuesta de la plataforma.
+ 
+procesarPagoConMercadoLibre(): Procesa el pago mediante el API de Mercado Libre, gestionando los tokens de autenticación y actualizando el estado según la respuesta.
+
+
+
+
+
+"""
