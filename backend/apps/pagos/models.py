cf0ea0c7943dc3ed37f4bf161e4675721bc89a78
aguilar <al217149@alumnos.uacj.mx>
2024-11-14 22:20:57 -0700

    Se ha agregador la implimentacion de metodos de pagos

--- backend/apps/pagos/models.py
+++ backend/apps/pagos/models.py
@@ -51,3 +51,45 @@ procesarPagoConMercadoLibre(): Procesa el pago mediante el API de Mercado Libre,
 
 
 """
+"""
+Clase Pago
+ 
+Atributos:
+ 
+	idPago (int): Identificador único del pago.
+	metodoDePago (str): Método de pago utilizado, como PayPal o Mercado Libre.
+	monto (float): Monto total del pago.
+	estado (str): Estado actual del pago (ejemplo: pendiente, completado, fallido).
+	idUsuario (Usuario): Referencia al usuario que realiza el pago.
+Métodos:
+ 
+procesarPago():
+	Realiza el procesamiento del pago digital mediante el método especificado en el atributo metodoDePago.
+	Si el método es PayPal, invoca el método procesarPagoConPayPal().
+	Si el método es Mercado Libre, invoca el método procesarPagoConMercadoLibre().
+ 
+verificarPago(): Verifica si el pago fue exitoso consultando la respuesta de la plataforma (PayPal o Mercado Libre) y actualiza el estado en consecuencia.
+ 
+procesarPagoConPayPal(): Implementa el procesamiento de pago a través de la API de PayPal, incluyendo:
+	Redirección al sitio de PayPal para autenticación.
+	Confirmación del pago y actualización del estado según la respuesta.
+ 
+procesarPagoConMercadoLibre(): Implementa el procesamiento de pago a través de la API de Mercado Libre, incluyendo:
+	Autenticación mediante token en Mercado Libre.
+	Confirmación del pago y actualización del estado según la respuesta de la plataforma.
+ 
+Implementación de Métodos Digitales de Pago:
+ 
+agregarMetodoDePago():
+	Método que permite agregar una nueva plataforma de pago digital en el sistema, configurando los datos de autenticación y el proceso específico de cada nueva 	plataforma.
+ 
+procesarPagoPersonalizado(metodo):
+	Realiza el procesamiento para una plataforma digital personalizada utilizando una estructura de plugins o configuración específica para nuevos métodos.
+Relación con la clase Pedido:
+ 
+Relación uno a uno entre Pedido y Pago: Un pedido puede tener solo un pago asociado, y cada pago corresponde a un único pedido específico.
+
+
+
+
+"""
