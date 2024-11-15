1f88d16e0cce4da657876b228c3f128a739e2f25
226129 <al226129@alumnos.uacj.mx>
2024-11-15 12:52:35 -0700

    Modelo de carrito implementado asi como sus metodos y conexiones con otros modelos

--- backend/apps/catalogo/models.py
+++ backend/apps/catalogo/models.py
@@ -27,4 +27,28 @@ Conexiones
     Pedido: 
         Conexion de 1 a muchos donde un producto puede estar en un solo pedido y un pedido puede tener muchos productos
 
+
+Modelo de Carrito
+
+Atributos
+    - idCarrito: Llave primaria, entero
+    - usuario: Usuario
+    - listaProductos: lista de Producto
+    - subtotal: Float
+    
+Metodos 
+    + agregarProducto): Metodo que agrega un producto al carrito
+    + eliminarProducto(): Metodo que elimina un producto del carrito
+    + calcularSubtotal(): Metodo que calcula el subtotal del carrito
+    + vaciarCarrito(): Metodo que permite eliminar por completo los productos del carrito
+    + obtenerTotal(): Metodo que obtiene el total de la compra
+    
+Conexiones
+    Usuario:
+        Conexion de 1 a 1 donde un carrito pertenece a un solo usuario y un usuario tiene un solo carrito
+    Producto:
+        Conexion de muchos a muchos donde un carrito puede tener muchos productos y muchos productos pueden estar en muchos carritos
+    Pedido: 
+        Conexion de 1 a muchos donde un carrito puede tener muchos pedidos y un pedido pertenece a un solo carrito
+        
 """
